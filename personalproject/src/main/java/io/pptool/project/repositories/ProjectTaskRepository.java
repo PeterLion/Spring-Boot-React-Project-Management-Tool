4c85dca74a352832be7eba477cd2eba562a4a997
Pierre Celestin NSHIMYUMUREMYI <nshimiyepeter@gmail.com>
2019-06-16 12:31:38 -0600

    I have finished the Project CRUD in REact. Started the New features in Spring

---
+++ personalproject/src/main/java/io/pptool/project/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package io.pptool.project.repositories;
+
+import io.pptool.project.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask,Long> {
+}
--- personalproject/src/main/java/io/pptool/project/services/ProjectService.java
+++ personalproject/src/main/java/io/pptool/project/services/ProjectService.java
@@ -1,7 +1,9 @@
 package io.pptool.project.services;
 
+import io.pptool.project.domain.Backlog;
 import io.pptool.project.domain.Project;
 import io.pptool.project.exceptions.ProjectIdException;
+import io.pptool.project.repositories.BacklogRepository;
 import io.pptool.project.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +13,21 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private BacklogRepository backlogRepository;
+
     public Project saveOrUpdateProject(Project project){
         try{
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            if (project.getId() ==null){
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+            if (project.getId() != null){
+               project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+            }
             return projectRepository.save(project);
         }catch (Exception e){
            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
